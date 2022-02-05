4627b8d14eeba03c0fafb754a64a270addbc6ab5
oscarmauriciogiraldo <oscardan@utp.edu.co>
2022-02-05 14:14:25 -0500

    inicio spring

---
+++ crud/src/test/java/co/edu/tu/isc/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.tu.isc.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
