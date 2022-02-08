52d74d53f9be16306c391378e4ce611db6f8b000
oscarmauriciogiraldo <oscardan@utp.edu.co>
2022-02-07 23:22:37 -0500

    devolver cadena peticion get localhost

--- crud/src/main/java/co/edu/tu/isc/crud/CrudApplication.java
+++ crud/src/main/java/co/edu/tu/isc/crud/CrudApplication.java
@@ -2,12 +2,35 @@ package co.edu.tu.isc.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+//main aplication
 @SpringBootApplication
+// se agregan nuevos elementos al main aplication
+/*nueva etiqueta o anotacion restcontroler --> indica que esta calse
+adicionalmente va a tener un servicio rest (rutas)
+*/
+@RestController
+// request maping hace el mapeo de las rutas el cual lo dirige a esta clase
+@RequestMapping("index")
 public class CrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
+        
+        // metodo get
+        // se mapea una peticion get a la siguiente ruta
+        @GetMapping("/{firstName}/{lastName}")
+        // llama la funcion 
+        public String sayIndex(
+                @PathVariable("firstName") String firstName, 
+                @PathVariable("lastName") String lastName){
+            return String.format("Index %s %s", firstName, lastName);
+            // retorna una cadena con nombre y apellido
+        }
 
 }
--- doc/README.md
+++ doc/README.md
@@ -1,10 +1,12 @@
 # Estructura Spring boot
 
 ## Proyecto Srping Boot
-    
+
     El archivo de configuracion de un proyecto maven es el archivo pomp.xml
 
-    lo principal de lo que se trata un proyecto spring, es que spring boot framework debe tener una etiqueta parent ( <parent> </parent>) en el archivo pomp.xml. Esta etiqueta quiere decir que el padre del proyecto o del cual este se esta heredando (Herencia del proyecto ) de donde se hereda.
+    lo principal de lo que se trata un proyecto spring, es que spring boot framework debe tener una etiqueta parent
+    ( <parent> </parent>) en el archivo pomp.xml.
+    Esta etiqueta quiere decir que el padre del proyecto o del cual este se esta heredando (Herencia del proyecto ) de donde se hereda.
 
     <parent>
         <groupId> Es un proyecto del framework spring boot  </groupId>
